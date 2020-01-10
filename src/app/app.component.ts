f633b8b981c79833cdb15aa75174d2354eec063b
Anthony Muli <mulianthony561@gmail.com>
2020-01-10 09:14:44 +0300

    add class for the Quote-app to act as the blueprint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  Quote = 'Do not go through life,grow through life';
+  quotes: string[];
+
+  constructor() {
+    this.quotes = [
+      "Do not go through life,grow through life",
+      "Life is really simple,but we insist on making it complicated",
+      "Do no harm but take no shit",
+      "Do one thing everyday that scares you",
+      "Dream big work hard make it happen",
+      "A little progress each day adds up to big results",
+      "Strive for progress not perfection"
+    ];
+  }
 }
