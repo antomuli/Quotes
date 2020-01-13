e9c7252980877094b29876f00843eb51d7b66768
Anthony Muli <mulianthony561@gmail.com>
2020-01-13 12:28:51 +0300

    Initialize app.quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,12 @@
 export class Quote {
+  public get id(): number {
+    return this._id;
+  }
+  public set id(value: number) {
+    this._id = value;
+  }
   public showAuthor: boolean;
-  constructor(public id: number,public name: string,public author: string,public completeDate: Date){
+  constructor(private _id: number,public quote: string,public author: string,public name: string,public completeDate: Date){
     this.showAuthor=false;
   }
 }
