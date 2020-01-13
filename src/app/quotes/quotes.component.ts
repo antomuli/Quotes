f613a7337c092d87aab62ab17acf352088b6aec0
Anthony Muli <mulianthony561@gmail.com>
2020-01-13 12:29:50 +0300

    initialize quotes.component.ts

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,45 +8,43 @@ import { Quote } from "../quote";
 export class QuotesComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(
-      1,
-      "Do not go through life grow through life",
-      "Eric Butterworth",
+      1,"Do not go through life grow through life","Eric Butterworth",'Nyota', 
       new Date(1980, 3, 14)
-    ),
+    ) ,
     new Quote(
       2,
       "Life is really simple,but we insist on making it complicated",
-      "Confucius",
+      "Confucius",'Lenny',
       new Date(1990, 8, 14)
     ),
     new Quote(
       3,
       "Do no harm but take no shit",
-      "Moira Fowley-Doyle",
+      "Moira Fowley-Doyle","Dennis",
       new Date(2000, 5, 14)
     ),
     new Quote(
       4,
       "Do one thing everyday that scares you",
-      "Eleanor Roosevelt",
+      "Eleanor Roosevelt","Macharia",
       new Date(2006, 7, 14)
     ),
     new Quote(
       5,
       "Dream big work hard make it happen",
-      "Zander Creation",
+      "Zander Creation","Anto",
       new Date(2008, 9, 14)
     ),
     new Quote(
       6,
       "A little progress each day adds up to big results",
-      "Jameel Mohsin",
+      "Jameel Mohsin","Muliooo",
       new Date(2018, 12, 14)
     ),
     new Quote(
       7,
       "Strive for progress not perfection",
-      "Daniel Amen",
+      "Daniel Amen","Rummy",
       new Date(2015, 11, 14)
     )
   ];
